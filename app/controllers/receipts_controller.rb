4285e3e7b9288f372a9161f7212ad552f4b5a682
Andy McBeath <mcbeath1@gmail.com>
2022-12-09 08:49:00 -0600

    Added to integer for all of the information being read for points

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -6,23 +6,24 @@ class ReceiptsController < ApplicationController
 
   def points
     points += retailer.length
-    if total[-1] == 0 && total[-2] == 0
+    if total[-1].to_i == 0 && total[-2].to_i == 0
       points += 50
-    elsif total[-1] == 5 && total[-2] == 2
+    elsif total[-1].to_i == 5 && total[-2].to_i == 2
       points += 25
-    elsif total[-1] == 0 && total[-2] == 5
+    elsif total[-1].to_i == 0 && total[-2].to_i == 5
       points += 25
-    elsif total[-1] == 7 && total[-2] == 5
+    elsif total[-1].to_i == 7 && total[-2].to_i == 5
       points += 25
     end
     points += items.length / 2
     if shortDescription.gsub(/\s+/, "").length % 3
       points += .2
     end
-    if purchaseDate[-1].odd?
+    if purchaseDate[-1].to_i.odd?
       points += 6
     end
-    if purchaseTime[0] == 1 && purchaseTime[1] > 4 && purchaseTime[1] < 16
+    if purchaseTime[0].to_i == 1 && purchaseTime[1].to_i > 4 && purchaseTime[1].to_i < 6
       points += 10
   end
+  return points
 end
